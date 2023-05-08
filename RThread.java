c8b5709c2bd4f6b6d85a1fa222c1c95e054fc9a8
matthewchunqed <108086692+matthewchunqed@users.noreply.github.com>
2023-05-08 11:36:55 -0400

    Add files via upload

--- RThread.java
+++ RThread.java
@@ -1,18 +1,51 @@
-import java.util.concurrent.atomic.AtomicIntegerArray;
 public class RThread implements Runnable {
 
-    private AtomicIntegerArray checker;
+    private int regionMin;
+    private int regionMax;
+    private int[] checker = new int[1_000_000_000];
+    private HashMap<Integer, Float> map = new HashMap<Integer, Float>();
+    private float[] data;
+    private AtomicInteger index;
+    private int uniqueID;
 
-    public RThread(int regionMin, int regionMax, AtomicIntegerArray checker, float[] data) {
-        this.checker = checker;
-        for(int i=regionMin; i<regionMax; i++){
-            checker.incrementAndGet((int)(data[i]*16777216));
-        }
+    public RThread(float[] data, AtomicInteger index, int uniqueID) {
+		this.regionMin = regionMin;
+        this.regionMax = regionMax;
+        this.data = data;
+        this.index = index;
+        this.uniqueID = uniqueID;
     }
 
     public void run () {
 
-        
-        
+    
+    float temp;
+    for(int i=0; i<data.length; i++){
+        float big = 100_000_000_000;
+        temp = data[i]*big;
+        if(temp < ((float)(1_000_000_000))*uniqueID || temp > ((float)(1_000_000_000))*(uniqueID+1)){
+            continue;
+        }
+        int temp2 = (int)(temp)/100;
+        checker[temp2]++;
+        map.put(temp2, data[i]);
+    }
+
+    while(index.get() < uniqueID){
+        if(index.get() == uniqueID){
+            break;
+        }
+    }
+    int index = 0;
+    for(int i=0; i<checker.length; i++){
+        if(checker[i] > 0){
+            for(int j=0; j<checker[i]; j++){
+                data[index] = map.get();
+                index++;
+            }
+        }
+    }
+
+
     }
 }
